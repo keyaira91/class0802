32416f20c6d69ffea63faa643e910d10f6f1d015
keyaira91 <keyaira22@hotmail.com>
2021-08-02 19:35:54 -0400

    Clean Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'class0802';
 
-
-  colors = ["Teal", "Black", "Pink"]
 
 }
